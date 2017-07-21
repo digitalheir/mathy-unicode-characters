e473ee1f752ea9bd70cceb52a07f318236c2880f
Maarten <maartentrompper@freedom.nl>
2017-07-21 14:49:14 +0200

    Update app.tsx

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -7,9 +7,38 @@ import {normalizeStrings, unicodeList} from "mathy-unicode-characters";
 
 const mountPoint = document.getElementById("mount-point");
 
+function parseUrlParams(): any {
+    const urlParams: any = {};
+
+    const regex = /([^&=]+)=?([^&]*)/g;
+    const decode = (s: string) => decodeURIComponent(s.replace(/\s+/g, ""));
+    const query = window.location.search.substring(1);
+
+    let match;
+    while (match = regex.exec(query))
+        urlParams[decode(match[1])] = decode(match[2]);
+
+    return urlParams;
+}
+const urlParams = parseUrlParams();
+
+const defaultShowOptions = defaultOptions;
+
+if(urlParams.latex) defaultShowOptions.latex = urlParams.latex !== "false"
+if(urlParams.wolfram) defaultShowOptions.wolfram = urlParams.wolfram !== "false"
+if(urlParams.aip) defaultShowOptions.aip = urlParams.aip !== "false"
+if(urlParams.acs) defaultShowOptions.acs = urlParams.acs !== "false"
+if(urlParams.afii) defaultShowOptions.afii = urlParams.afii !== "false"
+if(urlParams.ams) defaultShowOptions.ams = urlParams.ams !== "false"
+if(urlParams.aps) defaultShowOptions.aps = urlParams.aps !== "false"
+if(urlParams.bmp) defaultShowOptions.bmp = urlParams.bmp !== "false"
+if(urlParams.ieee) defaultShowOptions.ieee = urlParams.ieee !== "false"
+if(urlParams.springer) defaultShowOptions.springer = urlParams.springer !== "false"
+
+
 if (mountPoint)
     render(
-        <UnicodeApp defaultShowOptions={defaultOptions} chars={unicodeList/*.slice(0, 50)*/.map(char => {
+        <UnicodeApp defaultShowOptions={defaultShowOptions} query={urlParams.q} chars={unicodeList/*.slice(0, 50)*/.map(char => {
             return {
                 normalizedStrings: normalizeStrings(char),
                 char
